23be2e9a8990e6e86c79005bf4c4d39c987da4b9
pavel-056 <pavel.semyonov.2605@yandex.ru>
2020-12-21 17:48:01 +0900

    ui1

--- git yellow/main.py
+++ git yellow/main.py
@@ -1,15 +1,42 @@
 import sys
-from PyQt5 import uic
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from random import randint
 
-class MyWidget(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-        uic.loadUi('Ui.ui', self)
+        QMainWindow.__init__(self)
+
+        self.setMinimumSize(QSize(500, 500))
+        self.setWindowTitle("Circle")
+
+        button = QPushButton('Нажми', self)
+        button.clicked.connect(self.draw)
+        button.resize(100, 50)
+        button.move(250, 250)
+        self.line = QLine()
+
+    def paintEvent(self,event):
+        QMainWindow.paintEvent(self, event)
+        if not self.line.isNull():
+            painter = QPainter(self)
+            pen = QPen(Qt.yellow, 3)
+            painter.setPen(pen)
+            painter.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+            x, y = randint(0, 300), randint(0, 300)
+            painter.drawEllipse(x, x, y, y)
+
+    def draw(self):
+        button = self.sender()
+        self.line = QLine(QPoint(), button.pos())
+        self.update()
 
 
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
+    mainWin = MainWindow()
+    mainWin.show()
     sys.exit(app.exec_())
