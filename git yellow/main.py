2d588c4f2ed515f4a2f7a3eef3a5ff3cae954f1b
pavel-056 <pavel.semyonov.2605@yandex.ru>
2020-12-21 17:18:37 +0900

    ui1

--- git yellow/main.py
+++ git yellow/main.py
@@ -1,1 +1,15 @@
 import sys
+from PyQt5 import uic
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Ui.ui', self)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
